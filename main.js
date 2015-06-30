9f17f55565464df418aea2c46b6861a4a2b175ed
Jim D <heyjim89@gmail.com>
2015-06-30 13:47:11 -0600

    Made it live

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // 	 __  / /\__ \/ / // /_   / /   / /   / / / / /   / ,<   
 // 	/ /_/ /___/ / /__  __/  / /___/ /___/ /_/ / /___/ /| |  
 // 	\____//____/_/  /_/     \____/_____/\____/\____/_/ |_|  
-// 	                                                        
+                                                       
 $(function() {
 
 	// What I'll need:
@@ -62,4 +62,45 @@ $(function() {
 	// final append to container
 	$('.container').append(clock);
 
+
+
+	setInterval('updateClock()', 1000);
+
+
 });
+
+// 	    __    _____    ________
+// 	   / /   /  _/ |  / / ____/
+// 	  / /    / / | | / / __/   
+// 	 / /____/ /  | |/ / /___   
+// 	/_____/___/  |___/_____/   
+// 	                           
+
+//Pulled from jQuery clock demo
+
+function updateClock() {
+	var currentTime = new Date();
+	var currentHours = currentTime.getHours();
+	var currentMinutes = currentTime.getMinutes();
+	var currentSeconds = currentTime.getSeconds();
+
+	// Pad the minutes and seconds with leading zeros, if required
+	currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+	currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+	// Choose either "AM" or "PM" as appropriate
+	var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+
+	// Convert the hours component to 12-hour format if needed
+	currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+
+	// Convert an hours component of "0" to "12"
+	currentHours = (currentHours == 0) ? 12 : currentHours;
+
+	// Compose the string for display
+	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+
+
+	$(".clock-text").html(currentTimeString);
+
+}
