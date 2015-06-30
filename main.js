fcae9b50b011d0cf0843f75949255b9f538038b0
Jim D <heyjim89@gmail.com>
2015-06-30 15:58:13 -0600

    Using Adams toLocaleTimeString method and works great

--- main.js
+++ main.js
@@ -64,7 +64,8 @@ $(function() {
 
 
 
-	setInterval('updateClock()', 1000);
+	setInterval(updateClock, 1000);
+	// updateClock();
 
 
 });
@@ -79,28 +80,28 @@ $(function() {
 //Pulled from jQuery clock demo
 
 function updateClock() {
-	var currentTime = new Date();
-	var currentHours = currentTime.getHours();
-	var currentMinutes = currentTime.getMinutes();
-	var currentSeconds = currentTime.getSeconds();
+	// var currentTime = new Date();
+	// var currentHours = currentTime.getHours();
+	// var currentMinutes = currentTime.getMinutes();
+	// var currentSeconds = currentTime.getSeconds();
 
-	// Pad the minutes and seconds with leading zeros, if required
-	currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-	currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+	// // Pad the minutes and seconds with leading zeros, if required
+	// currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+	// currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
 
-	// Choose either "AM" or "PM" as appropriate
-	var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+	// // Choose either "AM" or "PM" as appropriate
+	// var timeOfDay = (currentHours < 12) ? "AM" : "PM";
 
-	// Convert the hours component to 12-hour format if needed
-	currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+	// // Convert the hours component to 12-hour format if needed
+	// currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
 
-	// Convert an hours component of "0" to "12"
-	currentHours = (currentHours == 0) ? 12 : currentHours;
+	// // Convert an hours component of "0" to "12"
+	// currentHours = (currentHours == 0) ? 12 : currentHours;
 
-	// Compose the string for display
-	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+	// // Compose the string for display
+	// var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
 
-
-	$(".clock-text").html(currentTimeString);
+	var date = new Date();
+	$(".clock-text").html(date.toLocaleTimeString().slice(0,8));
 
 }
